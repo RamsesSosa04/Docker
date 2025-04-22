226ab191ade76e709987aca4552ebedc84d62f28
RamsesSosa04 <sosaramses08@gmail.com>
2025-04-21 23:58:26 -0700

    Deleted names

--- app/app.py
+++ app/app.py
@@ -9,8 +9,6 @@ connection = mysql.connector.connect (
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS usuarios (id INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(255))")
 cursor.execute("INSERT INTO usuarios (nombre) VALUES ('Roman')")
-cursor.execute("INSERT INTO usuarios (nombre) VALUES ('Jose')")
-cursor.execute("INSERT INTO usuarios (nombre) VALUES ('Thomas')")
 connection.commit
 
 cursor.execute("SELECT * FROM usuarios")
