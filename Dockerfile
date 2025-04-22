29456ea32554eaf301b381d70cae369f1c27b196
RamsesSosa04 <sosaramses08@gmail.com>
2025-04-21 21:03:45 -0700

    Added docker structure

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+#Imagen base de python
+FROM python:3.10
+
+#Es para crear un directorio de trabajo dentro del contenedor 
+WORKDIR /app
+
+#Para copiar archivos
+COPY app/ app/
+
+#Instalar dependencias
+RUN pip install -r requirements.txt
+
+# ejecuta el script cuando inicie el contenedor
+CMD ["python", "app.py"]
---
+++ app/app.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+connection = mysql.connector.connect (
+    host = "ls-ef4300d410ebc1a8304030b638b03ed2da87f5c1.c90u2ss6wl6d.us-east-2.rds.amazonaws.com",
+    user = "dbmasteruser",
+    password = "SicnoLab2025",
+    database = "dbmastertest"
+)
+cursor = connection.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS usuarios (id INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(255))")
+cursor.execute("INSERT INTO usuarios (nombre) VALUES ('Roman')")
+connection.commit
+
+cursor.execute("SELECT * FROM usuarios")
+for file in cursor.fetchall():
+    print(file)
+
+cursor.close()
+connection.close()
