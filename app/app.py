a88b89a3d99d27bbd9e96ca1fe5e0c89b0afa0c0
Ramses Sosa <144416569+RamsesSosa04@users.noreply.github.com>
2025-05-09 23:49:26 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 connection = mysql.connector.connect (
-    host = "ls-ef4300d410ebc1a8304030b638b03ed2da87f5c1.c90u2ss6wl6d.us-east-2.rds.amazonaws.com",
+    host = "",
     user = "dbmasteruser",
     password = "SicnoLab2025",
     database = "dbmastertest"
